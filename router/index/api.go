698a4bd21da42c222f6b24c2705b7ae6174da7a0
yela <2067930913@qq.com>
2019-05-29 19:25:18 +0800

    :poop: edit log

--- router/index/api.go
+++ router/index/api.go
@@ -29,13 +29,15 @@ func (a *ApiController) Response(httpCode, errCode int, data gin.H) {
 	}
 	msg := conf.GetMsg(errCode)
 	beginTime, _ := strconv.ParseInt(a.C.Writer.Header().Get("X-Begin-Time"), 10, 64)
-
+	
 	duration := time.Now().Sub(time.Unix(0, beginTime))
 	milliseconds := float64(duration) / float64(time.Millisecond)
 	rounded := float64(int(milliseconds*100+.5)) / 100
 	roundedStr := fmt.Sprintf("%.3fms", rounded)
 	a.C.Writer.Header().Set("X-Response-time", roundedStr)
-	zgh.ZLog().Info("message", "Index Response", "code", errCode, "errMsg", msg, "took", roundedStr)
+	requestUrl := a.C.Request.URL.String()
+	requestMethod := a.C.Request.Method
+	zgh.ZLog().Info("message", "Index Response","Request Url",requestUrl,"Request method",requestMethod, "code", errCode, "errMsg", msg, "took", roundedStr)
 	if errCode == 500 {
 		a.C.HTML(http.StatusOK,"5xx.tmpl",data)
 	} else if errCode == 404 {
